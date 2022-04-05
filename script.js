7935fae4eb915812a2d2adf27c7f0a7fe45e08b0
denzel33t <denzel.eggerue@gmail.com>
2022-04-05 14:43:59 +0100

    more js stuff

---
+++ script.js
@@ -0,0 +1,15 @@
+`use strict`
+
+console.log(`HELLO WORLD!`)
+console.info(`this is a JS example.`)
+console.warn(`Danger.`)
+console.error(`This is an error.`)
+
+console.log(`%c` + `this is styling`, `color: red; background-color: white; font-size: 10px; padding: 5px;`);
+
+
+for(let A = 100; A < 200; A++) {
+    console.log(`A`);
+}
+
+
